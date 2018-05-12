0ca556be86556de96bdff5e182558b4f2b94eb96
Gökay Atay <gokay.atay@hacettepe.edu.tr>
2018-05-12 19:41:59 +0300

    Added Main Menu & Initial Normal Match

---
+++ Space Shooter/Assets/Space Shooter/Scripts/Main Menu/LaserScriptMini.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserScriptMini : MonoBehaviour {
+
+    public float speed = 25.0f;
+
+
+    private float maxHeight;
+
+    // Use this for initialization
+    void Start () {
+        maxHeight = Camera.main.orthographicSize + 50.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0.0f, speed * Time.deltaTime, 0.0f);
+
+        if (transform.position.y > maxHeight)
+            Destroy(gameObject);
+
+	}
+
+
+}
